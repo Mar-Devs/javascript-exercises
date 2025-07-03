3941798acc24eeffe94cecdd9c11933f60bd629d
Mar Devs <marah.mh50@gmail.com>
2025-07-03 16:44:04 +0300

    solve exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str,i) {
+    count =0;
+    do{
+        for(count; count < i; count++){
+            console.log(str)
+        }
 
+    }while(count !=i)
 };
+let str = "hi"
+console.log(str,3)
 
-// Do not edit below this line
-module.exports = repeatString;
+// // Do not edit below this line
+// module.exports = repeatString;

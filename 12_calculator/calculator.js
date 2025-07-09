7850caf9aa3c32793428bde506c0aa5429b3cff9
Mar Devs <marah.mh50@gmail.com>
2025-07-09 18:41:53 +0300

    solve exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  num1 = Number(num1)
+  num2 = Number(num2)
+	let result = num1 + num2
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+  let result = num1 - num2
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  let result = 0
+  for(let j = 0; j<num.length; j++){
+    result = result + num[j]
+  }
+  console.log(result)
+  return result
 };
 
-const multiply = function() {
+const multiply = function(num) {
+   let result = 1
+    for(let i = 0 ; i < num.length; i++){
+        result *= num[i]
+    }
+    return result
+};
+
+const power = function(num1,num2) {
+	num1 = Number(num1)
+  num2 = Number(num2)
+	let result = num1 ** num2
+  return result
+};
+
+const factorial = function(num) {
+  let result = num
+  if(num > 0){
+  for(let i = 1; i < num; i++){
+    result *= i
+  }
+}
+ else if(num == 0){
+  result = 1
+ }
+  return result
 
 };
 
-const power = function() {
-	
-};
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
